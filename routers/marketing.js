74e9fc5de5aea3c29535695559b79dd9a74cdfc2
Rafael Trias <rafatrias.27@gmail.com>
2025-06-20 03:18:06 -0400

    add marketing router and index.http

--- routers/marketing.js
+++ routers/marketing.js
@@ -39,4 +39,55 @@ routerMarketing.get('/:tema/:nivel', (req, res) => {
     res.send(JSON.stringify(resultados));
 });
 
+// Ruta para agregar un nuevo curso de marketing
+
+routerMarketing.post('/',(req, res) => {
+    let cursoNuevo = req.body;
+    marketing.push(cursoNuevo);
+    res.send(JSON.stringify(marketing));
+ });
+
+ // Ruta para actualizar un curso de marketing por ID
+
+ routermarketing.put('/:id', (req, res) => {
+  const cursoActualizado = req.body; // Obtiene el curso actualizado del cuerpo de la solicitud
+
+  const id = req.params.id; // Obtiene el ID del curso a actualizar desde los parámetros de la ruta
+
+  const indice = marketing.findIndex(curso => curso.id == id); // Busca el índice del curso por ID
+
+  if (indice >= 0) {
+    marketing[indice] = cursoActualizado; // Actualiza el curso en el índice encontrado
+  }
+  res.json(marketing);
+});
+
+// Ruta para modificar parcialmente un curso de marketing por ID
+
+routermarketing.patch('/:id', (req, res) => {
+  const infoNueva = req.body;
+  const id = req.params.id; // Obtiene el ID del curso a modificar desde los parámetros de la ruta
+
+  const indice = marketing.findIndex(curso => curso.id == id);
+
+  if (indice >= 0) {
+    const cursoAModificar = marketing[indice];
+    Object.assign(cursoAModificar, infoNueva); // Modifica el curso con la nueva información
+  }
+  res.json(marketing);
+});
+
+// Ruta para eliminar un curso de marketing por ID
+
+routermarketing.delete('/:id', (req, res) => {
+  const id = req.params.id;
+    // Obtiene el ID del curso a eliminar desde los parámetros de la ruta
+  const indice = marketing.findIndex(curso => curso.id == id);
+
+  if (indice >= 0) {
+    marketing.splice(indice, 1); // Elimina el curso del array
+  }
+  res.json(marketing);
+});
+
 module.exports = routerMarketing;
